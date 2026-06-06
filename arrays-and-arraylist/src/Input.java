b79498515abcb1726fbcde2742f76e7730d09135
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-06 20:30:02 +0530

    what happen if we don't use tostring method to print the array

--- arrays-and-arraylist/src/Input.java
+++ arrays-and-arraylist/src/Input.java
@@ -18,20 +18,21 @@ public class Input {
 
 //        easy way to print the array
         System.out.println(Arrays.toString(arr));
+        System.out.println(arr);
 
 //        input using for loops
-        for (int i = 0; i < 5; i++) {
-            arr[i] = input.nextInt();
-        }
+//        for (int i = 0; i < 5; i++) {
+//            arr[i] = input.nextInt();
+//        }
 
 //        for (int i = 0; i < 5; i++) {
 //            System.out.print(arr[i] + " ");
 //        }
 
-        for(int i : arr){
-            System.out.print(i + " ");
-        }
-        System.out.println(" ");
+//        for(int i : arr){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println(" ");
 
 //        System.out.println(arr[6]); //index out of bound error
 
