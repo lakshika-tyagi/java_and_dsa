060cb95229f0f9716b90859c4c6f98fd492da1d5
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-08 13:44:19 +0530

    output in arrays

--- arrays-and-arraylist/src/MultiDimension.java
+++ arrays-and-arraylist/src/MultiDimension.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MultiDimension {
     static void main() {
@@ -7,6 +8,8 @@ public class MultiDimension {
           4 5 6
           7 8 9
         */
+
+        Scanner in = new Scanner(System.in);
 
 
 //        syntax
@@ -27,13 +30,48 @@ public class MultiDimension {
 
 //        rows having variable no. of columns
         int[][] arr2 = {
-                {1,2,3,4,5,6,7,8},
-                {9},
+                {1,2,3,4,5,6,7},
+                {8,9},
                 {10,20,30,40,50}
         };
         System.out.println(Arrays.toString(arr2));
         System.out.println(arr2[1][0]);
 
+
 //        input of 2d arrays
+//        int[][] arr3 = new int[3][2];
+//
+//        for(int r = 0; r < arr3.length; r++){
+////            now, we take every row
+//            for(int c = 0; c < arr3[r].length; c++){
+//                arr3[r][c] = in.nextInt();
+//            }
+//        }
+
+//        System.out.println("variable columns");
+//        input for variable columns
+//        for(int r = 0; r < arr2.length; r++){
+////            now, we take every row
+//            for(int c = 0; c < arr2[r].length; c++){
+//                arr2[r][c] = in.nextInt();
+//            }
+//        }
+
+//        printing arrays
+
+//        using loops
+        for (int[] value : arr2) {
+            for (int i : value) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+//        using method
+        System.out.println(Arrays.deepToString(arr2));
+
+        for (int[] ints : arr2) {
+            System.out.println(Arrays.toString(ints));
+        }
     }
 }
