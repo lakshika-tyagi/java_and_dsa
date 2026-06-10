61279c99e97db2b2cdb4302c7ebc4a78d1bfb80b
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-10 18:57:44 +0530

    binary search

--- 07_binary-search/src/Main.java
+++ 07_binary-search/src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    static void main() {
+        int [] arr = {12,30,45,61,75,82,95,100,110,450,895};
+        System.out.println(binarySearch(arr,61));
+        System.out.println(binarySearch(arr,82));
+        System.out.println(binarySearch(arr,895));
+        System.out.println(binarySearch(arr,1));
+        System.out.println(binarySearch(arr,12));
+        System.out.println(binarySearch(arr,16));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    }
+
+    static int binarySearch(int [] arr, int target){
+        int low = 0;
+        int high = arr.length;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 }
