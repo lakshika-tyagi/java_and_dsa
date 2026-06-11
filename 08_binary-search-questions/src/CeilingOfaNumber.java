4ca0ebfff212bddcba3ef1ac5b523897445a3478
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-11 16:32:31 +0530

    ceiling of a number

--- 08_binary-search-questions/src/CeilingOfaNumber.java
+++ 08_binary-search-questions/src/CeilingOfaNumber.java
@@ -2,24 +2,14 @@ public class CeilingOfaNumber {
     static void main() {
         int [] arr = {2,3,5,9,14,16,18};
         System.out.println(ceiling(arr,0));
-        System.out.println(ceiling(arr,1));
         System.out.println(ceiling(arr,2));
         System.out.println(ceiling(arr,3));
         System.out.println(ceiling(arr,4));
         System.out.println(ceiling(arr,5));
         System.out.println(ceiling(arr,6));
-        System.out.println(ceiling(arr,7));
-        System.out.println(ceiling(arr,8));
-        System.out.println(ceiling(arr,9));
-        System.out.println(ceiling(arr,10));
-        System.out.println(ceiling(arr,11));
-        System.out.println(ceiling(arr,12));
         System.out.println(ceiling(arr,13));
-        System.out.println(ceiling(arr,14));
         System.out.println(ceiling(arr,15));
-        System.out.println(ceiling(arr,16));
         System.out.println(ceiling(arr,17));
-        System.out.println(ceiling(arr,18));
         System.out.println(ceiling(arr,20));
 
     }
