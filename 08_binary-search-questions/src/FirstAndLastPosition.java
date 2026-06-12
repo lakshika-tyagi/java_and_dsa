8d3e5150481446f83740c1c34f196ae272a38ada
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-12 22:30:02 +0530

    first and last position

---
+++ 08_binary-search-questions/src/FirstAndLastPosition.java
@@ -0,0 +1,16 @@
+public class FirstAndLastPosition {
+    static void main() {
+
+    }
+
+    static void getFirst(int [] arr,int start, int end, int target){
+        while()
+    }
+    static void searchRange(int [] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int mid =
+        }
+    }
+}
