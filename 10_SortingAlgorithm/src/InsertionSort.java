b3e05d8683d58045573d0c8edcae053f6220c111
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-19 19:34:47 +0530

    insertion sort

--- 10_SortingAlgorithm/src/InsertionSort.java
+++ 10_SortingAlgorithm/src/InsertionSort.java
@@ -13,11 +13,13 @@ public class InsertionSort {
         int n = arr.length;
         for(int i = 0; i < n - 1; i++){
             for(int j = i + 1; j > 0; j--){
-                if(arr[j] > arr[j-1])break;
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                }
+                else{
+                    break;
                 }
             }
         }
