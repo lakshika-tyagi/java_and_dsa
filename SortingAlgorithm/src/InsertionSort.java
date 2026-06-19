11a073cb982d24d5678153d358a6f3943bbbf451
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-19 19:27:22 +0530

    insertion sort

--- SortingAlgorithm/src/InsertionSort.java
+++ SortingAlgorithm/src/InsertionSort.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class InsertionSort {
     static void main() {
+        int[] arr = {5,3,4,1,2};
+        System.out.println(Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+
+    static void insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j > 0; j--){
+                if(arr[j] > arr[j-1])break;
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
     }
 }
