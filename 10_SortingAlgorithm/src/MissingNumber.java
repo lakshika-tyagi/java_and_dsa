c3e0c1dce350d8c5411be1d3fa2fa6f3a20292e8
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-23 13:11:14 +0530

    sorting the array

--- 10_SortingAlgorithm/src/MissingNumber.java
+++ 10_SortingAlgorithm/src/MissingNumber.java
@@ -2,7 +2,21 @@ public class MissingNumber {
     static void main() {
 
     }
-
+    static void sortArray(int [] arr){
+        int n = arr.length;
+        int i = 0;
+        while(i < n){
+            if(arr[i] == n || arr[i] == i){
+                i++;
+            }
+            else{
+                int req_idx = arr[i];
+                int temp = arr[i];
+                arr[i] = arr[req_idx];
+                arr[req_idx] = temp;
+            }
+        }
+    }
 
 
 }
