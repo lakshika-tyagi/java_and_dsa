2b0ed43286efeb87741f648cdf6778ce0b043140
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-24 17:27:02 +0530

    string pool and immutability

--- 11_Strings-and-StringBuilder/src/Main.java
+++ 11_Strings-and-StringBuilder/src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     static void main() {
+
+//        string literal
         String name = "Lakshika Tyagi";
         System.out.println(name);
+
+
         String a = "hello";
         String b = "hello";
+//        String pool : both a and b point to the same object "hello" in the heap memory , helps in optimization
+
+
         b = b + " !";
+//        string immutability : here b is assigned to new object, we haven't modified b as strings are immutable
+
+
         System.out.println(a + b);
+//        only b is changed , a has still the same value
     }
 }
