14912ed1873686b769ee454e9f111c6414d85459
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-24 17:23:25 +0530

    string pool and immutability

--- 11_Strings-and-StringBuilder/src/Main.java
+++ 11_Strings-and-StringBuilder/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     static void main() {
-
+        String name = "Lakshika Tyagi";
+        System.out.println(name);
+        String a = "hello";
+        String b = "hello";
+        b = b + " !";
+        System.out.println(a + b);
     }
 }
