10439aae6d96aeaf059c56b4df38a5d2f57f149b
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-26 15:52:09 +0530

    understanding .toString and printstream

--- 11_Strings-and-StringBuilder/src/Output.java
+++ 11_Strings-and-StringBuilder/src/Output.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class Output {
     static void main(String[] args) {
         System.out.println(56);
+        System.out.println("Lakshika Tyagi");
+        System.out.println(new int[] {2,3,4,5});
+        System.out.println(Arrays.toString(new int[] {2,3,4,5}));
+
+        String name = null;
+        System.out.println(name);
+
+        Integer num = 65;
+        System.out.println(num);
+        System.out.println(num.toString());
     }
 }
