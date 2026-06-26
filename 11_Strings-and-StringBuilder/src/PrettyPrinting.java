8ba8f2586394d3e64f575e38cc3d7626b3995e48
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-26 16:11:51 +0530

    pretty printing using format specifiers

---
+++ 11_Strings-and-StringBuilder/src/PrettyPrinting.java
@@ -0,0 +1,16 @@
+public class PrettyPrinting {
+    static void main() {
+        float a = 143.1234f;
+        System.out.printf("Formatted number is %.2f%n",a);
+
+        System.out.println(Math.PI);
+        System.out.printf("Pi : %.3f\n",Math.PI);
+
+        System.out.printf("My name is %s and I am %s.\n","Lakshika Tyagi","cool");
+
+        Integer num = 999;
+        System.out.printf("hex value of 999 is : %x\n",num.hashCode());
+        System.out.printf("Octal value of %d is %o\n",num,num);
+
+    }
+}
