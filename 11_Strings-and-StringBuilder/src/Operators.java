82a990063b583fe98a50679e5d2520862163b162
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-27 12:35:48 +0530

    operator overloading : exception + in strings

--- 11_Strings-and-StringBuilder/src/Operators.java
+++ 11_Strings-and-StringBuilder/src/Operators.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Operators {
     static void main() {
         System.out.println('a' + 'b');
@@ -8,6 +10,13 @@ public class Operators {
 //        int will be converted to its wrapper class Integer that will call toString() method
 //        this is after same steps as "a" + "1"
 
+//        string concatenate with objects
+        System.out.println("Lakshika" + new ArrayList<>());
+        System.out.println("Lakshika" + new Integer(34));
 
+//        object concatenate with objects
+//        System.out.println(new Integer(45) + new ArrayList<>());  //error : operator cannot be applied when both are objects
+
+        System.out.println("" +new Integer(34) +  new int[]{} );
     }
 }
