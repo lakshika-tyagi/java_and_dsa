605ebb19b978661fa7c4e41e102d3a7af25d0635
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-29 16:22:56 +0530

    understanding time complexity

---
+++ 14_Complexity/src/Fibo.java
@@ -0,0 +1,13 @@
+public class Fibo {
+
+    static void main() {
+
+        System.out.println(fibo(10));
+
+    }
+
+    static int fibo(int n){
+        if(n < 2)return n;
+        return fibo(n-1) + fibo(n-2);
+    }
+}
