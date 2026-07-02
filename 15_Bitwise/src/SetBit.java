6f3f4d75a22ab9656302916459803d95a07315ce
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-02 17:17:24 +0530

    set ith bit

--- 15_Bitwise/src/SetBit.java
+++ 15_Bitwise/src/SetBit.java
@@ -1,9 +1,13 @@
 public class SetBit {
     static void main() {
 
+        System.out.println(setBit(10,1));
+        System.out.println(setBit(4,3));
+
+        System.out.println(setBit(1437,7)); //1437 = 10110011101  , 10111011101 = 1501
     }
 
-    static int setBit(int n){
-        return 0;
+    static int setBit(int n,int i){
+        return n | (1 << (i-1));
     }
 }
